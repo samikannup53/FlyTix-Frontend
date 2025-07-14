9b11573900d49716a6389e1d55f311cd521a2b18
samikannup53 <samikannup53@gmail.com>
2025-07-14 09:52:45 +0530

    Add Scroll To Top Utility

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ToastContainer } from "react-toastify";
 import UserRoutes from "./routes/UserRoutes";
 import { CookieBanner } from "./modules/user/components";
+import { ScrollToTop } from "./shared/utils/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <CookieBanner />
       <UserRoutes />
       <ToastContainer
